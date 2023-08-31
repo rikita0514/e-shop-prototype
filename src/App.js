af916882f6a095cf6b191121d0cc7c8a6c0498f5
rikita0514 <rikitamondal0514@gmail.com>
2023-08-31 20:20:33 +0530

    caterogy&css-edt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Category from './components/category';
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/categories")
+    .then(Response => Response.json())
+    .then(data => {
+      console.log(data);
+      setResults(data);
+    })
+  }, [])
+
+  const renderCategories = () => {
+    // return results.map(c => 
+    //   <Category key={c.id} id={c.id} title={c.title}/>
+    // )//Incase the results does not get called
+    const categories = [];
+    for(let i=0; i < results.length;i++){
+      categories.push(<Category key={results[i].id} id={results[i].id} title={results[i].title}/>)
+    }
+    return categories;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>My Store</header>
+
+      <section>
+        <nav>
+        {results && renderCategories()}
+        </nav>
+        <article>
+          main area
+        </article>
+      </section>
+
+      <footer>
+        footer
+      </footer>
+    </>
   );
 }
 
