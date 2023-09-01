8c4863b8b818eb7a881a8108b88e4b52f4d230a6
rikita0514 <rikitamondal0514@gmail.com>
2023-09-02 00:56:18 +0530

    fetcher-edit-done

--- src/App.js
+++ src/App.js
@@ -6,57 +6,57 @@ import { fetcher } from './fetcher';
 import Category from './components/category';
 
 function App() {
-  const [categories, setCategories] = useState({errorMessage: '', data: []});
-  const [products, setProducts] = useState({errorMessage: '', data: []});
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const responseObject = await fetcher("/categories");
-      setCategories(responseObject);
+      const data = await fetcher("/categories");
+      debugger;
+      setCategories(data);
     }
     fetchData();
+    
+    }, [])
 
-  }, [])
-
-  const handleCategoryClick = id =>{
-    const fetchData = async () => {
-      const responseObject = await fetcher("/products?catId=" + id);
-      setProducts(responseObject);
+    const handleCategoryClick = id => {
+      fetch("http://localhost:3001/products?catId=" + id)
+      .then(Response => Response.json())
+      .then(data => {
+      console.log(data);
+      setProducts(data);
+      })
     }
-    fetchData();
-  }
 
-  const renderCategories = () => {
-    return categories.data.map(c => 
-      <Category key={c.id} id={c.id} title={c.title} onCategoryClick={()=> handleCategoryClick(c.id)}/>
-    )
-  }
+    const renderCategories = () => {
+      return categories.map(c =>
+        <Category key={c.id} id={c.id} title={c.title}  onCategoryClick={() => handleCategoryClick(c.id)}/>
+      );
+    }
 
-  const renderProducts = () => {
-    return products.data.map(p =>
-      <div>{p.title}</div>
-      )
-  }
+    const renderProducts = () => {
+      return products.map(p => 
+        <div>{p.title}</div>
+        )
+    }
 
   return (
     <>
-      <header>My Store</header>
+    <header>My Store</header>
+    <section>
+      <nav>
+        { categories && renderCategories()  }
+      </nav>
+      <article>
+        <h1>Products</h1>
+        { products && renderProducts() }
+      </article>
+    </section>
 
-      <section>
-        <nav>
-          { categories.errorMessage && <div>Error: {categories.errorMessage}</div>}
-          { categories.data && renderCategories() }
-        </nav>
-        <article>
-        { products.errorMessage && <div>Error: {products.errorMessage}</div>}
-          <h1>Products</h1>
-          { products && renderProducts() }
-        </article>
-      </section>
-
-      <footer>
-        footer
-      </footer>
+    <footer>
+      footer
+    </footer>
     </>
   );
 }
--- src/fetcher.js
+++ src/fetcher.js
@@ -1,16 +1,8 @@
-const BASE_URL = "http://localhost3001";
+const BASE_URL = "http://localhost:3001";
 
 export const fetcher = async (url) => {
-    let responseObject = { errorMessage: '', data: [] };
-
-    try{
-    const Response = await fetch(BASE_URL + url)
-    const responseData = await Response.json();
-    responseObject.errorMessage = '';
-    responseObject.data = responseData;
-    }
-    catch (err) {
-        responseObject.errorMessage = err.message;
-    }
-    return responseObject;
+    const Response = await fetch(BASE_URL + url);
+    const ResponseData = await Response.json();
+    return ResponseData;
+    
 };
