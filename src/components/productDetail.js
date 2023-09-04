bb553b2249d600be74282e6536bf23066ed3fdb0
rikita0514 <rikitamondal0514@gmail.com>
2023-09-05 02:40:18 +0530

    productDetail-edit

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { useParams } from 'react-router-dom'
+
+import { getProductById } from '../fetcher';
+
 const ProductDetail = () => {
+    const [product, setProduct] = React.useState({errorMessage: '', data: []});
+    const params = useParams();
+
+    React.useEffect(() => {
+        const fetchData =async () => {
+            const responseObject = await getProductById(params.productId);
+            setProduct(responseObject);
+        }
+        fetchData();
+    }, [params.productId]);
+
+
   return (
-    <div>ProductDetail</div>
+    <div>ProductDetail id:{params.productId} title:{product.data.title}</div>
   )
 }
 
